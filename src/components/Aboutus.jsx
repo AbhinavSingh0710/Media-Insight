031f3f2fd8c19943360bf2728ab9d87a4358c95b
AbhinavSingh0710 <abhinav.singh0718@gmail.com>
2024-09-14 20:53:47 +0530

    About us section issue

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,33 +1,9 @@
-// import React from 'react'
-// import US from "/US.jpeg";
-// import { useNavigate } from 'react-router-dom';
+import React from 'react'
 
-// const Aboutus = () => {
-//     const navigate = useNavigate()
-//   return (
-//     <div className='h-screen w-screen overflow-hidden relative'>
-//          <div className='text-zinc-400 text-3xl absolute top-[2%] left-[2%]'>
-//          <i
-//             onClick={() => navigate(-1)}
-//             className="hover:text-[#6556CD] ri-arrow-left-line"
-//           ></i>Home
-//          </div>
-//         <h1 className='absolute left-[50%] top-[10%] transform -translate-x-1/2 transform -translate-y-1/2  text-white text-5xl'>I Love You Rishika</h1>
-//        <div className='flex items-center justify-center h-full w-full'>
-//        <div className='h-[60%] w-[40%] rounded-md bg-yellow-100 flex items-center justify-center gap-2 overflow-hidden'>
-//             <div className='h-full w-full'>
-//                 <img className='rounded-md h-full w-full object-cover object-center' src={US} alt="" />
-//             </div>
-//             <div className='w-[40%] '>
-//                 <h1 className='text-zinc-400 mb-1'>Jabse tumhai dekha hai,</h1>
-//                 <h1 className='text-zinc-400 mb-1'>hogaya hu mein deewana,</h1>
-//                 <h1 className='text-zinc-400 mb-1'>mujhese tum  aab karlo shaadi,</h1>
-//                 <h1 className='text-zinc-400 mb-1'>aur raha nahi jata.</h1>
-//             </div>
-//         </div>
-//        </div>
-//     </div>
-//   )
-// }
+const Aboutus = () => {
+  return (
+    <div>Aboutus</div>
+  )
+}
 
-// export default Aboutus
+export default Aboutus
